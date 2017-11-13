da734b839bbc366a526f6dcd385d0cfd185ad9ab
MellowGuy <thotyl88@gmail.com>
2017-11-13 11:44:29 -0500

    Setting up level switching

---
+++ Glitch Garden Project/Assets/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public float autoLoadNextLevelAfter;
+
+	public void Start()
+	{
+		Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	//Loads level based on string passed in.
+	public void LoadLevel(string name)
+	{
+		SceneManager.LoadScene(name);
+		Debug.Log("New Level load: " + name);
+		
+		//Application.LoadLevel(name);
+	}
+
+	public void LoadNextLevel()
+	{
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	//Quits application
+	public void QuitRequest()
+	{
+		Debug.Log("Quit requested");
+		Application.Quit();
+	}
+
+}
